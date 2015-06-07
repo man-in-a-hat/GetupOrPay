0847b15ab6afedf6499b29161de277e57f599434
taydakov <lev.taydakov@gmail.com>
2015-06-07 15:24:42 -0700

    Director is included

--- app.jsx
+++ app.jsx
@@ -1,14 +1,50 @@
 'use strict';
 
-var HelloMessage = React.createClass({
+var Menu = React.createClass({
   render: function() {
     return (
-      <h1>{this.props.text}</h1>
+      <div>
+        <a href="#/page1">Page1</a>
+        |
+        <a href="#/page2">Page2</a>
+      </div>
     );
   }
 });
 
-React.render(
-  <HelloMessage text="Hello, world!" />,
-  document.getElementById('container')
-);
+var TextMessage = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Menu />
+        <h1>{this.props.text}</h1>
+      </div>
+    );
+  }
+});
+
+function Page1() {
+
+  React.render(
+    <TextMessage text="Page #1" />,
+    document.getElementById('container')
+  );
+
+}
+
+function Page2() {
+
+  React.render(
+    <TextMessage text="Page #2" />,
+    document.getElementById('container')
+  );
+
+}
+
+var routes = {
+  '/page1': Page1,
+  '/page2': Page2
+};
+
+var router = Router(routes);
+router.init('/page1');
