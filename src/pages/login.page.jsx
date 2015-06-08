a2a0af5b8834361477ee610b012516932048b6c0
taydakov <lev.taydakov@gmail.com>
2015-06-07 22:32:47 -0700

    Login page implemented

--- src/pages/login.page.jsx
+++ src/pages/login.page.jsx
@@ -1,12 +1,43 @@
 'use strict';
 
 var React = require('react');
-var TextMessage = require('../components/textmessage.react');
+var Input = require('react-bootstrap').Input;
+var Button = require('react-bootstrap').Button;
+var Row = require('react-bootstrap').Row;
+var Col = require('react-bootstrap').Col;
+
+var LoginPage = React.createClass({
+
+  render: function() {
+    return (
+      <div className="container vcenter">
+        <Row>
+          <Col xs={6}>
+            <Button bsStyle="primary" block>Login</Button>
+          </Col>
+          <Col xs={6}>
+            <Button block>Signup</Button>
+          </Col>
+        </Row>
+        <form>
+          <Input type="text" placeholder="Email Address" />
+          <Input type="text" placeholder="Password" />
+        </form>
+        <Row>
+          <Col xsOffset={3} xs={6}>
+            <Button bsStyle="primary" bsSize="large" block>Login</Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+
+});
 
 module.exports = function Login() {
 
   React.render(
-    <TextMessage text="Log in page" />,
+    <LoginPage />,
     document.getElementById('container')
   );
 
