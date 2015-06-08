7e0932c30823df6b22b55309e1afabd89789b09e
taydakov <lev.taydakov@gmail.com>
2015-06-08 00:09:35 -0700

    Sketch for destination page

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,13 @@ var Router = require('director').Router;
 var LoginPage = require('./pages/login.page');
 var SignupPage = require('./pages/signup.page');
 var PaymentPage = require('./pages/payment.page');
+var DestinationPage = require('./pages/destination.page');
 
 var routes = {
   '/login': LoginPage,
   '/signup': SignupPage,
   '/payment': PaymentPage
+  // '/destination': DestinationPage
 };
 
 var router = Router(routes);
